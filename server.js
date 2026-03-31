6c29c2c6ff3ad3a921c736aa950baa6089528a76
Mamta Kumari Gupta <72861695+mamtakitw@users.noreply.github.com>
2026-03-31 18:07:49 +0530

    fix port for render

--- server.js
+++ server.js
@@ -67,6 +67,8 @@ app.get("/info", async (req, res) => {
 });
 
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000 🚀");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} 🚀`);
 });
